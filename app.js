02786cfe562d94944aeaadcff7d7c045eddee301
Mariano Baliño <marianobalinho@gmail.com>
2022-04-11 22:20:15 -0300

    sprint 2: estructura del proyecto

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+  console.log("Servidor funcionando")
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+})
+
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/views/login.html');
+})
+
+app.get('/register', (req, res) => {
+  res.sendFile(__dirname + '/views/register.html');
+})
+app.get('/productCart', (req, res) => {
+  res.sendFile(__dirname + '/views/productCart.html');
+})
+
+app.get('/productDetail', (req, res) => {
+  res.sendFile(__dirname + '/views/productDetail.html');
+})

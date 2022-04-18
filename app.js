b86856fa766b0676292b79d6c80c3937cdd3fb26
Mariano Baliño <marianobalinho@gmail.com>
2022-04-18 01:25:54 -0300

    agregar hero con carrousel y productos

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use(express.static("node_modules"));
 
 app.listen(3000, () => {
-  console.log("Servidor funcionando")
-})
+  console.log("Servidor funcionando");
+});
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
-app.get('/login', (req, res) => {
-  res.sendFile(__dirname + '/views/login.html');
-})
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/views/login.html");
+});
 
-app.get('/register', (req, res) => {
-  res.sendFile(__dirname + '/views/register.html');
-})
-app.get('/productCart', (req, res) => {
-  res.sendFile(__dirname + '/views/productCart.html');
-})
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/views/register.html");
+});
+app.get("/productCart", (req, res) => {
+  res.sendFile(__dirname + "/views/productCart.html");
+});
 
-app.get('/productDetail', (req, res) => {
-  res.sendFile(__dirname + '/views/productDetail.html');
-})
+app.get("/productDetail", (req, res) => {
+  res.sendFile(__dirname + "/views/productDetail.html");
+});
---
+++ public/js/carousel.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  loop: true,
+
+  autoplay: {
+    delay: 5000,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});

ed4aa3a8b17721b3ec679def8434b4b2a43716ee
Mariano Baliño <marianobalinho@gmail.com>
2022-05-08 03:49:43 -0300

    add and apply partials, nav-is-scrolled

--- public/js/script.js
+++ public/js/script.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const openBtn = document.querySelector(".btn-open");
   const closeBtn = document.querySelector(".btn-close");
   const mobileMenu = document.querySelector(".mobile-menu");
+  const navbar = document.querySelector(".navbar");
 
   const toggleMobileMenu = () => {
     mobileMenu.classList.toggle("is-open");
@@ -11,4 +12,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
   openBtn.addEventListener("click", toggleMobileMenu);
   closeBtn.addEventListener("click", toggleMobileMenu);
+
+  window.addEventListener('scroll', () => {
+    const navbarHeight = navbar.clientHeight; 
+    if (window.scrollY > navbarHeight) {
+      navbar.classList.add('is-scrolled');
+    } else {
+      navbar.classList.remove('is-scrolled')
+    }
+  })
 });

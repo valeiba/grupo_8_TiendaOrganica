5da07d080a738334c5a013afaea947f0cb6e9591
valeiba <valeibanez21@gmail.com>
2022-06-26 20:11:31 -0300

    subo express validator1

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,10 +6,21 @@ const usersFilePath = path.join(__dirname, "/../database/users.json");
 const users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const {validationResult}= require ('express-validator');
 
 const controller = {
   register: (req, res) => {
-    res.render("./users/register");
+    res.render("users/register");
+  },
+  processRegister: (req,res)=>{
+    const resultValidation= validationResult(req);
+    if(resultValidation.errors.length>0)
+    return res.render('users/register', {
+      errors:resultValidation.mapped(),
+      oldData:req.body
+    })
+
+
   },
   createUser: async (req, res) => {
     const {name, lastname, email, password, role} = req.body;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controllers/users");
+const controller = require("../controllers/userController");
 const { validateRegister, validateLogin } = require("../middlewares/auth.js");
+const {body}= require ('express-validator');
+const validations=[
+    body('name').notEmpty().withMessage('tiene que ingresar un nombre'),
+    body('lastname').notEmpty().withMessage('tiene que ingresar un apellido'),
+    body('email').notEmpty().withMessage('tiene que ingresar un email').bail().isEmail().withMessage('debes escribir un formato de correo valido'),
+    body('passWord').notEmpty().withMessage('tiene que ingresar una contraseña')
+]
 
 router.get("/register", controller.register);
-router.post("/register", validateRegister, controller.createUser);
+router.post("/register", validations, controller.processRegister)
 router.get("/login", controller.login);
 router.post("/login", validateLogin, controller.processLogin);
 

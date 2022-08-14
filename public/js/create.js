9fd92874fa4c3a45041ac2d2681997f21b7429ca
Mariano Baliño <marianobalinho@gmail.com>
2022-08-14 17:03:28 -0300

    update validations

--- public/js/create.js
+++ public/js/create.js
@@ -1,39 +1,66 @@
 window.onload = function () {
-  let formulario = document.querySelector("form#form");
+  let formulario = document.querySelector("#form");
   formulario.addEventListener("submit", function (event) {
     let errores = [];
+
     let campoNombre = document.querySelector("input#name");
     if (campoNombre.value === "") {
-      errores.push("El campo nombre es obligatorio ");
+      errores.push("El nombre es obligatorio");
       campoNombre.classList.add("is-invalid");
     } else if (campoNombre.value.length < 5) {
-      errores.push("el campo nombre debe tener al menos 5 caracteres");
+      errores.push("El nombre debe tener al menos 5 caracteres");
       campoNombre.classList.add("is-invalid");
     } else {
       campoNombre.classList.remove("is-invalid");
       campoNombre.classList.add("is-valid");
-      campoNombre.focus();
     }
-    let campoDescripcion = document.querySelector("input#description");
+
+    let campoPrecio = document.querySelector("input#price");
+    if (campoPrecio.value === "") {
+      errores.push("El precio es obligatorio");
+      campoPrecio.classList.add("is-invalid");
+    } else {
+      campoPrecio.classList.remove("is-invalid");
+      campoPrecio.classList.add("is-valid");
+    }
+
+    let campoStock = document.querySelector("input#stock");
+    if (campoStock.value === "") {
+      errores.push("El stock es obligatorio");
+      campoStock.classList.add("is-invalid");
+    } else {
+      campoStock.classList.remove("is-invalid");
+      campoStock.classList.add("is-valid");
+    }
+
+    // let campoOnSale = document.querySelector("#on_sale");
+    // if (campoOnSale.value === "") {
+    //   errores.push("Completá si el producto está en oferta o no");
+    //   campoOnSale.classList.add("is-invalid");
+    // } else {
+    //   campoOnSale.classList.remove("is-invalid");
+    //   campoOnSale.classList.add("is-valid");
+    // }
+
+    let campoDescripcion = document.querySelector("textarea#description");
     if (campoDescripcion.value.length < 20) {
-      errores.push(" El campo de descripcion debe tener al menos 20 caracteres");
+      errores.push("La descripción debe tener al menos 20 caracteres");
       campoDescripcion.classList.add("is-invalid");
     } else {
       campoDescripcion.classList.remove("is-invalid");
       campoDescripcion.classList.add("is-valid");
-      campoDescripcion.focus();
     }
-    let campoImagen = document.querySelector("input#image");
 
-    var allowedExtensions = /(.jpg|.jpeg|.png|.gif)$/i;
+    let campoImagen = document.querySelector("input#image");
+    const allowedExtensions = /(.jpg|.jpeg|.png|.gif)$/i;
     if (!allowedExtensions.exec(campoImagen.value)) {
       errores.push("Extensión no permitida. Utiliza: .jpeg/.jpg/.png/.gif.");
       campoImagen.value = "";
     } else {
       campoImagen.classList.remove("is-invalid");
       campoImagen.classList.add("is-valid");
-      campoImagen.focus();
     }
+
     if (errores.length > 0) {
       event.preventDefault();
       let erroresInput = document.querySelector("div.errores ul");

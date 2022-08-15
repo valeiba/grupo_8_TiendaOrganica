2e0377ec7bb067b88c4b31581b3ced9beef64d70
Mariano Baliño <marianobalinho@gmail.com>
2022-08-14 21:17:14 -0300

    rework create/edit product view, add related products by category (detail.ejs), add search for products (logic WIP), modify data.sql

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -3,7 +3,11 @@ const db = require("../database/models");
 const home = async (req, res) => {
   try {
     const products = await db.Product.findAll({
+      where: {
+        on_sale: 1
+      }
     })
+    console.log(products)
     return res.render("index", {
       products
     });
